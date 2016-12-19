72d1149135dfc94fd7917c4e4a5bf858ac67fec9
Katz Ueno <katz@concrete5.co.jp>
2016-12-20 00:24:02 +0900

    Remove phpmyadmin installation since Apache is no longer installed on Cloud9

--- cloud9-install-mautic.bash
+++ cloud9-install-mautic.bash
@@ -19,16 +19,12 @@
 DOWNLOAD_URL="https://github.com/mautic/mautic/archive/2.4.0.zip"
 DESTINATION="/home/ubuntu/workspace"
 VERSION="2.4.0"
-INSTALL_PHPMYADMIN="yes"
 INSTALL_LEMP="yes"
 LEMP_NGINX_CONFIG="https://raw.githubusercontent.com/katzueno/mautic-install-shell/master/conf/cloud9-nginx.conf"
 
 # EXEC
 # ----------
 cd ${DESTINATION}
-if [ "$INSTALL_PHPMYADMIN" = "yes" ]; then
-    phpmyadmin-ctl install
-fi
 if [ "$INSTALL_LEMP" = "yes" ]; then
     curl -L https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/install.sh | bash
     sudo rm -f /etc/nginx/sites-enabled/c9
